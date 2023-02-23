443e6e3b35d829260ac164cc0527f8128824fa04
Artyom-Gerchik <bombruchelyator@gmail.com>
2023-02-24 00:42:50 +0300

    in progress

--- LAB1/LAB1/Program.cs
+++ LAB1/LAB1/Program.cs
@@ -6,51 +6,88 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+public class Message
+{
+    public DateTime Time { get; set; }
+    public string SenderName { get; set; }
+    public string Text { get; set; }
+
+    public Message(DateTime time, string name, string text)
+    {
+        Time = time;
+        SenderName = name;
+        Text = text;
+    }
+}
+
 public static class Program
 {
     public static async Task<int> Main()
     {
+        var acceptedMessages = new List<Message>();
+        var sentMessages = new List<Message>();
+
         IPAddress localAddress = IPAddress.Parse("127.0.0.1");
-        Console.Write("Введите свое имя: ");
-        string? username = Console.ReadLine();
-        Console.Write("Введите порт для приема сообщений: ");
+        var username = "";
+        //var acceptPort = 0;
+        //var sendPort = 0;
+
+        Console.Write("Username: ");
+        username = Console.ReadLine();
+
+        Console.Write("Port for incoming messages: ");
         if (!int.TryParse(Console.ReadLine(), out var localPort)) return 0;
-        Console.Write("Введите порт для отправки сообщений: ");
+
+        Console.Write("Port for sending messages: ");
         if (!int.TryParse(Console.ReadLine(), out var remotePort)) return 0;
+
         Console.WriteLine();
 
-        // запускаем получение сообщений
-        Task.Run(ReceiveMessageAsync);
-        // запускаем ввод и отправку сообщений
-        await SendMessageAsync();
+        Task.Run(ReceiveMessageAsync); // get messages in real-time
 
-        // отправка сообщений
+        await SendMessageAsync(); // send messages in real-time
+
         async Task SendMessageAsync()
         {
             using Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
-            // отправляем сообщения
+
+            Console.WriteLine("Enter message: ");
             while (true)
             {
-                //Console.Write("MESSAGE: ");
-                var message = Console.ReadLine(); // сообщение для отправки
-                // если введена пустая строка, выходим из цикла и завершаем ввод сообщений
+                var message = Console.ReadLine();
+
                 if (string.IsNullOrWhiteSpace(message)) break;
-                // иначе добавляем к сообщению имя пользователя
-                message = $"{username}: {message}";
+
+                sentMessages.Add(new Message(DateTime.Now, username, message));
+
+                message = $"|{DateTime.Now}|#{username}#%{message}%";
                 byte[] data = Encoding.UTF8.GetBytes(message);
                 // и отправляем на 127.0.0.1:remotePort
                 await sender.SendToAsync(data, SocketFlags.None, new IPEndPoint(localAddress, remotePort));
+                Console.Clear();
+
+                foreach (var item in acceptedMessages)
+                {
+                    Console.Write("\t\t\t\t\t\t\t");
+                    Console.WriteLine($"{item.SenderName}: {item.Text}");
+                }
+
+                foreach (var item in sentMessages)
+                {
+                    Console.WriteLine($"You: {item.Text}");
+                }
             }
         }
 
         // отправка сообщений
         async Task ReceiveMessageAsync()
         {
-            byte[] data = new byte[65535]; // буфер для получаемых данных
-            // сокет для прослушки сообщений
-            using Socket receiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            // запускаем получение сообщений по адресу 127.0.0.1:localPort
+            byte[] data = new byte[65535];
+
+            using Socket
+                receiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                    ProtocolType.Udp); // socket for listening
+
             receiver.Bind(new IPEndPoint(localAddress, localPort));
             while (true)
             {
@@ -58,7 +95,69 @@ public static class Program
                 var result = await receiver.ReceiveFromAsync(data, SocketFlags.None, new IPEndPoint(IPAddress.Any, 0));
                 var message = Encoding.UTF8.GetString(data, 0, result.ReceivedBytes);
                 // выводим сообщение
-                Console.WriteLine($" {message}");
+
+                var tmpMessage = message;
+
+                var readingTime = false;
+                var readingUsername = false;
+                var readingMessage = false;
+                var time = new DateTime();
+                var username = "";
+                var messageTMP = "";
+                var tmp = "";
+
+                foreach (var character in tmpMessage)
+                {
+                    if (character == Convert.ToChar("|") && !readingTime) // start reading time
+                    {
+                        readingTime = true;
+                    }
+                    else if (character == Convert.ToChar("|") && readingTime) // end reading time
+                    {
+                        time = Convert.ToDateTime(tmp);
+                        readingTime = false;
+                        tmp = "";
+                    }
+                    else if (character == Convert.ToChar("#") && !readingUsername) // start reading time
+                    {
+                        readingUsername = true;
+                    }
+                    else if (character == Convert.ToChar("#") && readingUsername) // end reading time
+                    {
+                        username = tmp;
+                        readingUsername = false;
+                        tmp = "";
+                    }
+                    else if (character == Convert.ToChar("%") && !readingMessage) // start reading time
+                    {
+                        readingMessage = true;
+                    }
+                    else if (character == Convert.ToChar("%") && readingMessage) // end reading time
+                    {
+                        messageTMP = tmp;
+                        readingMessage = false;
+                        tmp = "";
+                    }
+                    else
+                    {
+                        tmp += character;
+                    }
+                }
+
+                acceptedMessages.Add(new Message(time, username, messageTMP));
+
+                Console.Clear();
+
+                foreach (var item in acceptedMessages)
+                {
+                    Console.Write("\t\t\t\t\t\t\t");
+                    Console.WriteLine($"{item.SenderName}: {item.Text}");
+                }
+
+                foreach (var item in sentMessages)
+                {
+                    Console.WriteLine($"You: {item.Text}");
+                }
             }
         }
 
